e456ae0c4059fa56ee508a82ee9108e421176b0a
TusharSaini999 <tusharsaini.in@gmail.com>
2025-04-10 14:40:36 +0530

    Add a Q

--- Q83.java
+++ Q83.java
@@ -1,4 +1,29 @@
-/// A
-/// CCC
-/// EEEEE
-/// FFFFFFFFF
+///      *
+///     * *
+///    * * *
+///   * * * *
+import java.util.Scanner;
+
+class Q83{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a No:");
+        int n=s.nextInt();
+        int cou=1;
+        for(int i=n;i>0;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=cou;j++){
+                System.out.print("*");
+                if(j==cou){
+                    System.out.println();
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            cou++;
+        }
+    }
+}
--- Q95.java
+++ Q95.java
@@ -1,4 +1,23 @@
 /// 5
+/// 54
 /// 543
 /// 5432
 /// 54321
+import java.util.Scanner;
+
+class Q95{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a No:");
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            int cout=n;
+            for(int j=1;j<=i;j++){
+                System.out.print(cout--);
+                if(j==i){
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
